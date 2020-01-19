f29c98c08231500cd47e5cb05fe940d0f7ab86e9
Peter Dobbs <dobbsp8@gmail.com>
2020-01-19 15:53:27 -0600

    Update detect_circles.py

--- detect_circles.py
+++ detect_circles.py
@@ -4,16 +4,49 @@ from imutils.perspective import four_point_transform
 from imutils import contours
 import cv2
 
-page_file = 'special/temp/page_1.png'
-img = cv2.imread(page_file)
+RED = (0, 0, 255)
+BLUE = (255, 0, 0)
+GREEN = (0, 255, 0)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-bwImage = cv2.threshold(
-    gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-cv2.imshow('black and white image', cv2.resize(
-    bwImage, None, fx=.15, fy=.15))
+# read in image
+page_file = 'special/temp/page_1.png'
+file_img = cv2.imread(page_file)
+wThresh = 0.7
+hThresh = 0.0
+
+# restrict the contour detection to a scoring_region
+(rows, columns) = file_img.shape[:2]
+widthThreshold = int(columns*wThresh)
+heightThreshold = int(rows*hThresh)
+region = file_img[heightThreshold:rows, widthThreshold:columns]
+
+# process the image
+gray = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY)
+bwImg = cv2.threshold(
+    gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+# cv2.imshow('black and white image', cv2.resize(
+#     bwImg, None, fx=.5, fy=.5))
+# cv2.imwrite('bwImg_region.png', bwImg)
+# cv2.waitKey(0)
+
+# Hough Transform to detect circles
+circles = cv2.HoughCircles(bwImg, cv2.HOUGH_GRADIENT, dp=1,
+                           minDist=30, param1=5, param2=15,
+                           minRadius=10, maxRadius=20)
+circles = np.uint16(np.around(circles))
+print("number of detected circles:", len(circles[0, :]))
+for i in circles[0, :]:
+    # draw the outer circle
+    cv2.circle(region, (i[0], i[1]), i[2], BLUE, 2)
+    # draw the center of the circle
+    # cv2.circle(region, (i[0], i[1]), 2, RED, 3)
+
+cv2.imshow('detected circles', cv2.resize(
+    region, None, fx=.5, fy=.5))
+cv2.imwrite('detected_circles.png', region)
 cv2.waitKey(0)
 
-cnts, hierarchy = cv2.findContours(bwImage, cv2.RETR_EXTERNAL,
-                                   cv2.CHAIN_APPROX_SIMPLE)
-print("Number of detected contours:", len(cnts))
+# # find contours
+# cnts, hierarchy = cv2.findContours(bwImg, cv2.RETR_EXTERNAL,
+#                                    cv2.CHAIN_APPROX_SIMPLE)
+# print("Number of detected contours:", len(cnts))
